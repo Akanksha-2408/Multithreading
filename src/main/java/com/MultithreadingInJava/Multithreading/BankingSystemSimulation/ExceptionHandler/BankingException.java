b0da6e31f933a7a3fafc502af8c0bd68e02b2534
Akanksha-2408 <akanksha.senad@perennialsys.com>
2025-09-30 15:32:41 +0530

    BankingException.java

--- src/main/java/com/MultithreadingInJava/Multithreading/BankingSystemSimulation/ExceptionHandler/BankingException.java
+++ src/main/java/com/MultithreadingInJava/Multithreading/BankingSystemSimulation/ExceptionHandler/BankingException.java
@@ -1,8 +1,5 @@
 package com.MultithreadingInJava.Multithreading.BankingSystemSimulation.ExceptionHandler;
 
-//Error code- exception (error code: alphanumeric/ numeric- should not match generic http codes)
-//Study and use , Where and why use logs, loggers ?
-
 public class BankingException extends RuntimeException {
 
     private final int errorCode;
